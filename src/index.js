8e49d30514b2fc7c2d5fd52c613c41ab343ec464
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-09-27 17:39:21 +0100

    chore: setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import ComingSoon from "./pages/ComingSoon";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ComingSoon />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
