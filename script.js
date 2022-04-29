ef4769dfca917b3215b11e2a7f5cb95af4620127
mildrette <mildredfonka1@gmail.com>
2022-04-29 11:24:01 +0100

    fixed merged conflix

--- script.js
+++ script.js
@@ -36,3 +36,17 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+/*function for active navbar 
+
+var navitemContainer = document.getElementById("navbar");
+var navitem = navlinkContainer.getElementsByClassName("navlink");
+for (var i = 0; i < navlink.length; i++) {
+  link[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+*/
