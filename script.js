de818c70eb8af42326c208b91c25ac132c282634
mildrette <mildredfonka1@gmail.com>
2022-11-04 14:35:33 +0100

    added active navbar on click

--- script.js
+++ script.js
@@ -66,7 +66,21 @@ window.addEventListener("scroll", () => {
 
 
 
+// active navbar
+var btnContainer = document.getElementById("SupportedContent");
 
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+} 
 
 
 
